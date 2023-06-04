730e98376e910699c42830a1e1764adc74761d56
gustavoPresa <gustavopr89@gmail.com>
2023-06-04 12:29:05 -0300

    feat: Add BaseUserModel in provider model

--- stationery-store-provider/provider/models.py
+++ stationery-store-provider/provider/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db.models import CharField
+from django.db.models import DateTimeField
+from django.db.models import EmailField
+from django.db.models import Model
 
-# Create your models here.
+
+class BaseUserModel(Model):
+    name = CharField('Nome', max_length=50)
+
+    email = EmailField('E-mail', max_length=100, unique=True)
+
+    phone_number = CharField('Número de telefone', max_length=12)
+
+    created_at = DateTimeField('Data de Criação', auto_now_add=True)
+
+    updated_at = DateTimeField('Data de Atualização', auto_now=True)
--- stationery-store-provider/stationery_store_provider/settings.py
+++ stationery-store-provider/stationery_store_provider/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "provider",
 ]
 
 MIDDLEWARE = [
@@ -88,16 +89,22 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
