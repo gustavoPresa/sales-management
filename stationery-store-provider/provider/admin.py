16195eb05f050154aa2e4aa4d3e55948e57ed7ac
gustavoPresa <gustavopr89@gmail.com>
2023-06-04 20:55:56 -0300

    feat: Add admin to Product and CommissionConfig models

--- stationery-store-provider/provider/admin.py
+++ stationery-store-provider/provider/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
 from provider.models import Client
+from provider.models import CommissionConfig
+from provider.models import Product
 from provider.models import Seller
 
 
@@ -15,3 +17,26 @@ class ClientAdmin(ModelAdmin):
 class SellerAdmin(ModelAdmin):
     list_display = ('name', 'email', 'phone_number')
     search_fields = ('name', 'email',)
+
+
+@admin.register(CommissionConfig)
+class CommissionConfigAdmin(ModelAdmin):
+    list_display = (
+        'day',
+        'min_commission_percentage',
+        'max_commission_percentage'
+    )
+
+    search_fields = ('day',)
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = (
+        'description',
+        'product_code',
+        'unitary_value',
+        'commission_percentage'
+    )
+
+    search_fields = ('description', 'product_code',)
