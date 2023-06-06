2bad3730c582837395cac17861599f8a01c54469
gustavoPresa <gustavopr89@gmail.com>
2023-06-06 19:21:24 -0300

    feat: Add new routes and implement the Header component

--- stationery-store-dashboard/dashboard/src/App.js
+++ stationery-store-dashboard/dashboard/src/App.js
@@ -1,24 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Commissions from './pages/Commissions';
+import Sales from './pages/Sales';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Sales/>} />
+          <Route path='/vendas' element={<Sales/>} />
+          <Route path='/comissoes' element={<Commissions/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
